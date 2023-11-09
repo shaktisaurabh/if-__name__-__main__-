1c2823416894274a3fed2256aaa20259611cb5cf
shaktisaurabh <132367856+shaktisaurabh@users.noreply.github.com>
2023-11-09 16:30:40 +0530

    Add files via upload

---
+++ manny5.py
@@ -0,0 +1,12 @@
+def square(num):
+    print(num**2)
+
+def main():
+    for i in range(1,11):
+        square(i) 
+
+if __name__=="__main__":
+    main() 
+
+#if you want to give the other coder who imports your file, an option of being able to execute a piece of code
+#from his own file then we code like this 
